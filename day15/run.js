b8a3fbf09916cbea1a37e6df563535a53adfefaf
samstronghammer <sam@luna-nelson.com>
2024-12-15 22:15:30 -0500

    Day 15 cleaned up

--- day15/run.js
+++ day15/run.js
@@ -7,15 +7,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const input = InputUtils.toStringArray(__dirname)
-const boardArray = []
-let directions = ""
-input.forEach(line => {
-  if (line.startsWith("#")) {
-    boardArray.push(line)
-  } else if (line.length > 0) {
-    directions += line
-  }
-})
+const dirObj = { "^": UP, ">": RIGHT, "v": DOWN, "<": LEFT }
+const charToDir = (c) => {
+  if (c in dirObj) return dirObj[c]
+  throw new Error("invalid direction char")
+}
+
+const boardArray = input.filter(line => line.startsWith("#"))
+const directions = input.filter(line => !line.startsWith("#") && line).join("").split("").map(charToDir)
 const makeBoard = (boardArray) => {
   const board = InputUtils.arrayToPoint2DMap(boardArray)
   const startPosition = Point2D.fromString([...board.entries()].find(([_, value]) => value === "@")[0])
@@ -30,147 +29,82 @@ const [board2, startPosition2] = makeBoard(boardArray.map(line => {
     .replace(/@/g, "@.")
 }))
 
-const obj = { "^": UP, ">": RIGHT, "v": DOWN, "<": LEFT }
-const charToDir = (c) => {
-  if (c in obj) return obj[c]
-  throw new Error("invalid direction char")
-}
-
-let currPos = startPosition
-directions.split("").forEach(c => {
-  const dir = charToDir(c)
-  let nextPos = currPos.add(dir)
-  let nextChar = board.get(nextPos.toString())
-  switch (nextChar) {
-    case ".":
-      currPos = nextPos
-      break;
-    case "#":
-      break;
-    case "O":
-      while (nextChar === "O") {
-        nextPos = nextPos.add(dir)
-        nextChar = board.get(nextPos.toString())
-      }
-      if (nextChar === "#") break
-      if (nextChar === ".") {
-        currPos = currPos.add(dir)
-        board.set(nextPos.toString(), "O")
-        board.set(currPos.toString(), ".")
-      } else {
-        throw new Error("invalid next char")
-      }
-      break;
-    default:
-      throw new Error("invalid next char")
-  }
-})
-
-console.log([...board.entries()].reduce((acc, [key, value]) => {
-  if (value !== "O") return acc
-  const point = Point2D.fromString(key)
-  return acc + point.r * 100 + point.c
-}, 0))
-
-
-const canPushBox = (pos, dir) => {
+const canPushBox = (pos, dir, boardMap) => {
   const nextPos = pos.add(dir)
-  const nextChar = board2.get(nextPos.toString())
+  const nextChar = boardMap.get(nextPos.toString())
   switch (nextChar) {
     case ".":
       return true
     case "#":
       return false
+    case "O":
+      return canPushBox(nextPos, dir, boardMap)
     case "[":
-      if (dir.equals(RIGHT)) {
-        return canPushBox(nextPos.add(RIGHT), dir)
-      } else if (dir.equals(LEFT)) {
-        throw new Error("Can't push left on this")
-      } else {
-        return canPushBox(nextPos, dir) && canPushBox(nextPos.add(RIGHT), dir)
-      }
+      return (dir.equals(RIGHT) || canPushBox(nextPos, dir, boardMap)) && (dir.equals(LEFT) || canPushBox(nextPos.add(RIGHT), dir, boardMap))
     case "]":
-      if (dir.equals(RIGHT)) {
-        throw new Error("Can't push right on this")
-      } else if (dir.equals(LEFT)) {
-        return canPushBox(nextPos.add(LEFT), dir)
-      } else {
-        return canPushBox(nextPos, dir) && canPushBox(nextPos.add(LEFT), dir)
-      }
+      return canPushBox(pos.add(LEFT), dir, boardMap) // Defer to left side of block
     default:
-      console.log(nextChar)
-      throw new Error("invalid next char")
+      throw new Error(`invalid next char, canPushBox: ${nextChar}`)
   }
 }
 
-const doPushBox = (pos, dir) => {
+const doPushBox = (pos, dir, boardMap) => {
   const nextPos = pos.add(dir)
-  const nextChar = board2.get(nextPos.toString())
+  const nextChar = boardMap.get(nextPos.toString())
   switch (nextChar) {
     case ".":
       return
-    case "#":
-      throw new Error("invalid wall")
+    case "O":
+      doPushBox(nextPos, dir, boardMap)
+      boardMap.set(nextPos.toString(), ".")
+      boardMap.set(nextPos.add(dir).toString(), "O")
+      break
     case "[":
-      if (dir.equals(RIGHT)) {
-        doPushBox(nextPos.add(RIGHT), dir)
-        board2.set(nextPos.toString(), ".")
-        board2.set(nextPos.add(RIGHT).toString(), "[")
-        board2.set(nextPos.add(RIGHT).add(RIGHT).toString(), "]")
-      } else if (dir.equals(LEFT)) {
-        throw new Error("Can't push left on this")
-      } else {
-        doPushBox(nextPos, dir)
-        doPushBox(nextPos.add(RIGHT), dir)
-        board2.set(nextPos.toString(), ".")
-        board2.set(nextPos.add(RIGHT).toString(), ".")
-        board2.set(nextPos.add(dir).toString(), "[")
-        board2.set(nextPos.add(dir).add(RIGHT).toString(), "]")
-      }
+      !dir.equals(RIGHT) && doPushBox(nextPos, dir, boardMap) // if not pushing right, need to make room for this point
+      !dir.equals(LEFT) && doPushBox(nextPos.add(RIGHT), dir, boardMap) // if not pushing left, need to make room for other part of block
+      boardMap.set(nextPos.toString(), ".")
+      boardMap.set(nextPos.add(RIGHT).toString(), ".")
+      boardMap.set(nextPos.add(dir).toString(), "[")
+      boardMap.set(nextPos.add(dir).add(RIGHT).toString(), "]")
       break
     case "]":
-      if (dir.equals(LEFT)) {
-        doPushBox(nextPos.add(LEFT), dir)
-        board2.set(nextPos.toString(), ".")
-        board2.set(nextPos.add(LEFT).toString(), "]")
-        board2.set(nextPos.add(LEFT).add(LEFT).toString(), "[")
-      } else if (dir.equals(RIGHT)) {
-        throw new Error("Can't push right on this")
-      } else {
-        doPushBox(pos.add(LEFT), dir)
-      }
+      doPushBox(pos.add(LEFT), dir, boardMap) // Defer to left side of block
       break
     default:
-      console.log(nextChar)
-      throw new Error("invalid next char")
+      throw new Error(`invalid next char, doPushBox: ${nextChar}`)
   }
 }
 
-let currPos2 = startPosition2
-directions.split("").forEach(c => {
-  const dir = charToDir(c)
-  let nextPos = currPos2.add(dir)
-  let nextChar = board2.get(nextPos.toString())
-  switch (nextChar) {
-    case ".":
-      currPos2 = nextPos
-      break;
-    case "#":
-      break;
-    case "[":
-    case "]":
-      if (!canPushBox(currPos2, dir)) break
-      doPushBox(currPos2, dir)
-      currPos2 = nextPos
-      break
-    default:
-      console.log(nextChar)
-      throw new Error("invalid next char")
-  }
-})
+const doDirections = (start, boardMap) => {
+  let curr = start
+  directions.forEach(dir => {
+    const nextPos = curr.add(dir)
+    const nextChar = boardMap.get(nextPos.toString())
+    switch (nextChar) {
+      case ".":
+        curr = nextPos
+        break;
+      case "#":
+        break;
+      case "O":
+      case "[":
+      case "]":
+        if (!canPushBox(curr, dir, boardMap)) break
+        doPushBox(curr, dir, boardMap)
+        curr = nextPos
+        break
+      default:
+        throw new Error(`invalid next char, doDirections: ${nextChar}`)
+    }
+  })
+  return boardMap
+}
 
-console.log([...board2.entries()].reduce((acc, [key, value]) => {
-  if (value !== "[") return acc
+const scoreMap = (map) => [...map.entries()].reduce((acc, [key, value]) => {
+  if (value !== "O" && value !== "[") return acc
   const point = Point2D.fromString(key)
   return acc + point.r * 100 + point.c
-}, 0))
+}, 0)
+
+console.log(scoreMap(doDirections(startPosition, board)))
+console.log(scoreMap(doDirections(startPosition2, board2)))

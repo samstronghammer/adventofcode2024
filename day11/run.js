55f2bde7193b180b4882a98346729fb5756fba6e
samstronghammer <sam@luna-nelson.com>
2024-12-17 16:30:04 -0500

    Update day 11

--- day11/run.js
+++ day11/run.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const input = InputUtils.toIntArrays(__dirname)[0].map(n => BigInt(n))
+const input = InputUtils.toBigIntArrays(__dirname)[0]
 
 //number of blinks, input number => number of output rocks 
 //45,342483 => 2342434

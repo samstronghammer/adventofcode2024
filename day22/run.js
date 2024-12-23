62205bf7077bf7b0b44953c0e316a6f5b2b2ab18
samstronghammer <sam@luna-nelson.com>
2024-12-22 22:07:42 -0500

    Day 22 polish

--- day22/run.js
+++ day22/run.js
@@ -7,72 +7,45 @@ const __dirname = path.dirname(__filename);
 
 const input = InputUtils.toBigIntArrays(__dirname).map(arr => arr[0])
 
-const mix = (n1, n2) => {
-  return n1 ^ n2
-}
-
-const prune = (n) => {
-  return n % 16777216n // 2^24
-}
+const mix = (n1, n2) => n1 ^ n2
+const mod2to24 = 2n ** 24n - 1n // 23 ones.
+const prune = (n) => n & mod2to24
 
 const evolve = (n) => {
-  const n2 = prune(mix(n, n << 6n)) // 2^6
-  const n3 = prune(mix(n2, n2 >> 5n)) // 2^5
-  const n4 = prune(mix(n3, n3 << 11n)) // 2^11
-  return n4
-}
-
-const calcAllArrays = () => {
-  const arrs = []
-  for (let a = -9; a <= 9; a++) {
-    for (let b = -9; b <= 9; b++) {
-      for (let c = -9; c <= 9; c++) {
-        for (let d = -9; d <= 9; d++) {
-          arrs.push([a, b, c, d])
-        }
-      }
-    }
-  }
-  return arrs
+  n = prune(mix(n, n << 6n))
+  n = prune(mix(n, n >> 5n)) // 2^5
+  return prune(mix(n, n << 11n)) // 2^11
 }
 
 // "-1,-1,0,2" => total number of bananas gotten with this sequence
 const map = new Map()
 
-const allArrays = calcAllArrays()
-allArrays.forEach((arr) => {
-  map.set(arr.join(","), 0n)
-})
-
 const iterate = (n, iterations) => {
-  const numbers = [[n, n % 10n, undefined]]
+  const deltas = [undefined]
   let newN = n
   const bestMap = new Map()
   for (let i = 0; i < iterations; i++) {
+    const oldDigit = newN % 10n
     newN = evolve(newN)
-    numbers.push([newN, newN % 10n, newN % 10n - numbers.at(-1)[1]])
-    const last4 = numbers.slice(-4).map(arr => arr[2])
-    const last4Key = last4.join(",")
-    if (!map.has(last4Key) || bestMap.has(last4Key)) continue
-    bestMap.set(last4Key, numbers.at(-1)[1])
+    const newDigit = newN % 10n
+    deltas.push(newDigit - oldDigit)
+    const last4Key = deltas.slice(-4).join(",")
+    if (i < 3n || bestMap.has(last4Key)) continue
+    bestMap.set(last4Key, newDigit)
   }
-  [...bestMap.entries()].forEach(([key, value]) => {
-    map.set(key, map.get(key) + value)
+  bestMap.forEach((value, key) => {
+    map.set(key, (map.get(key) ?? 0n) + value)
   })
-  return numbers
+  return newN
 }
 
 console.log(input.reduce((acc, val) => {
-  return acc + iterate(val, 2000).at(-1)[0]
+  return acc + iterate(val, 2000)
 }, 0n).toString())
 
-let bestSequence = undefined;
 let bestValue = undefined;
-[...map.entries()].forEach(([key, value]) => {
-  if (!bestSequence || value > bestValue) {
-    bestSequence = key
-    bestValue = value
-    return
-  }
+map.forEach((value, _) => {
+  if (bestValue !== undefined && value < bestValue) return
+  bestValue = value
 })
 console.log(bestValue.toString())
